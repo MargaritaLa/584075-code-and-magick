2806791cbc0b8e2835e30f04b7b451b8387ee329
Маргарита Лаевская <terra_82@mail.ru>
2017-11-27 19:54:10 +0300

    module2-task1 - правки: /Разбей отрисовку гистограмм и базового текста на разные функции/

--- js/stat.js
+++ js/stat.js
@@ -1,49 +1,58 @@
-
 'use strict';
 
 (function () {
+
+  function drawingHistograms() {
+
+  }
+
+  function renderBasicText(ctx, beginningLine) {
+    ctx.fillStyle = '#333333';
+    ctx.strokeStyle = '#333333';
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура, вы победили!', beginningLine, 50);
+    ctx.fillText('Список результатов:', beginningLine, 70);
+  }
+
   window.renderStatistics = function(ctx, names, times) {
-    var max,
-    start,
-    widthColumn = 40,
-    heightHistogramme = 150,
-    leftPoint = 120,
-    topPoint = 100,
-    bottomWhiteRect = 270,
-    indentColumn = 50,
-    myColor = 'rgba(255, 0, 0, 1)';
+    var max;
+    var start;
+    var widthColumn = 40;
+    var heightHistogramme = 150;
+    var leftPoint = 120;
+    var topPoint = 100;
+    var bottomWhiteRect = 270;
+    var indentColumn = 50;
+    var myColor = 'rgba(255, 0, 0, 1)';
 
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
     ctx.fillRect(110, 20, 420, 270);
     ctx.fillStyle = 'white';
     ctx.fillRect(100, 10, 420, bottomWhiteRect);
-    ctx.fillStyle = "#333333";
-    ctx.strokeStyle = "#333333";
-    ctx.font = "16px PT Mono";
-    ctx.fillText("Ура, вы победили!", leftPoint, 50);
-    ctx.fillText("Список результатов:", leftPoint, 70);
+
+    renderBasicText(ctx, leftPoint);
 
     max = Math.max.apply(Math, times);
 
     heightHistogramme -= topPoint;
 
-    for(var i = 0; i <= names.length - 1; i++){
+    for (var i = 0; i <= names.length - 1; i++) {
 
-      var top =  heightHistogramme - Math.round( heightHistogramme * ( Math.round(times[i]) / max ) );
+      var top = heightHistogramme - Math.round(heightHistogramme * (Math.round(times[i]) / max));
 
-      start = i === 0 ? 0 : i * ( widthColumn + indentColumn );
+      start = i === 0 ? 0 : i * (widthColumn + indentColumn);
 
-      if(names[i] === 'Вы'){
+      if (names[i] === 'Вы') {
         ctx.fillStyle = myColor;
       } else {
-        ctx.fillStyle = ('rgba(0, 0, 255, '+ Math.random() +')');
+        ctx.fillStyle = ('rgba(0, 0, 255, ' + Math.random() + ')');
       }
 
-      ctx.fillRect( start + leftPoint, top + topPoint, widthColumn, heightHistogramme - top + topPoint);
-      ctx.fillStyle = "#333333";
+      ctx.fillRect(start + leftPoint, top + topPoint, widthColumn, heightHistogramme - top + topPoint);
+      ctx.fillStyle = '#333333';
       ctx.fillText(names[i], start + leftPoint, bottomWhiteRect);
 
     }
-  }
+  };
 })();
 
